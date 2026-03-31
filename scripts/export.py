cb8721e89b8d872e28a73f15d686c677b3615016
thijnhoekstra <t.w.hoekstra@tudelft.nl>
2026-03-31 10:57:13 +0200

    Improved export.py

--- scripts/export.py
+++ scripts/export.py
@@ -1,23 +1,27 @@
 import subprocess
-import os
-import re
-from pathlib import Path
 
-def run_tests_and_export_outputs(docs_folder="../docs", output_folder="outputs"):
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+def export_test_outputs(
+        docs_folder: Annotated[str, typer.Argument(help="Folder in which to search for Python files.")] = "../docs"):
     """
     Run all test files in `docs_folder`, capture print outputs per test case,
     and export them to a corresponding .txt file in `output_folder`.
     Each test's output is separated by section markers.
     """
-    # Ensure output folder exists
-    Path(output_folder).mkdir(parents=True, exist_ok=True)
 
     docs_folder = Path(docs_folder)
-    print(f"Finding test in folder {docs_folder}")
-    for test_file in docs_folder.rglob("*.py"):
-
-        print(f"Processing {test_file.name}")
+    print(f"Finding tests in folder {docs_folder}")
+    files = list(docs_folder.rglob("*.py"))
+    if len(files) == 0:
+        print("No tests found in folder")
+    for test_file in files:
+        print(f"Exporting {test_file.name}... ".ljust(40), end="")
         if test_file.name == "conftest.py":
+            print("Skipping...")
             continue
 
         # Determine the output file name
@@ -31,9 +35,12 @@ def run_tests_and_export_outputs(docs_folder="../docs", output_folder="outputs")
             check=False,
         )
 
+        print(f"Found and ran {len(result.stdout.split('--8<-- [start')) - 1} tests... ".ljust(40), end="")
+
         with open(output_file, "w") as f:
             f.write(result.stdout)
 
+        print(f"Finished writing to {output_file}")
 
 if __name__ == "__main__":
-    run_tests_and_export_outputs()
+    typer.run(export_test_outputs)
